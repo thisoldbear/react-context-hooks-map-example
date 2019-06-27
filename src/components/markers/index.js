42f5f5861ce727c81305162013a29865b6cbf163
thisoldbear <danielwilliams.design@gmail.com>
2019-06-27 09:27:37 +0100

    Refactor marker renderer

--- src/components/markers/index.js
+++ src/components/markers/index.js
@@ -4,15 +4,15 @@ import Marker from '../marker';
 
 const renderMarkers = (markers, map) =>
   markers.map(({ title, lat, lng, id }) =>
-    React.cloneElement(<Marker />, {
-      id,
-      title,
-      key: id,
-      map,
-      google: window.google,
-      lat,
-      lng
-    })
+    <Marker
+      id={id}
+      title={title}
+      key={id}
+      map={map}
+      google={window.google}
+      lat={lat}
+      lng={lng}
+    />
   );
 
 export default renderMarkers;
